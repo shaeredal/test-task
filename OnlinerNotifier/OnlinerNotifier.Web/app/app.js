eb6e291951b04effe4f452f08c5cd915d65a2163
shaeredal <shaeredal@gmail.com>
2016-06-15 17:24:02 +0300

    Frontend frameworks, bundles, scripts add.

--- OnlinerNotifier/OnlinerNotifier.Web/app/app.js
+++ OnlinerNotifier/OnlinerNotifier.Web/app/app.js
@@ -1,1 +1,13 @@
-﻿
+﻿var app = angular.module('OnlinerNotifier',
+[
+    'ngRoute',
+    'OnlinerNotifier.Auth'
+]);
+
+app.config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider.when('/auth', {
+            templateUrl: '/app/views/auth.html',
+            controller: 'AuthCtrl'
+        });
+}] )
