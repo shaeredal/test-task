91e3efaf875bc5182aa129d0ebb8e6514eb92d66
shaeredal <shaeredal@gmail.com>
2016-06-15 12:55:55 +0300

    Authorization controller set up. Angular add.

---
+++ OnlinerNotifier/OnlinerNotifier.Web/App_Start/AutofacConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+using OAuth2;
+
+namespace OnlinerNotifier
+{
+    public class AutofacConfig
+    {
+        public static void RegisterDependencies()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+
+            builder.RegisterModelBinders(Assembly.GetExecutingAssembly());
+            builder.RegisterModelBinderProvider();
+
+            builder.RegisterModule<AutofacWebTypesModule>();
+
+            builder.RegisterSource(new ViewRegistrationSource());
+
+            builder.RegisterFilterProvider();
+
+            builder.RegisterType<AuthorizationRoot>().AsSelf().SingleInstance();
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
--- OnlinerNotifier/OnlinerNotifier.Web/Controllers/AuthorizationController.cs
+++ OnlinerNotifier/OnlinerNotifier.Web/Controllers/AuthorizationController.cs
@@ -1,14 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
+using System.Web.Mvc;
+using OAuth2;
+using OAuth2.Client;
+using OnlinerNotifier.Models;
 
 namespace OnlinerNotifier.Controllers
 {
-    public class AuthorizationController : ApiController
+    public class AuthorizationController : Controller
     {
-        
+        private readonly AuthorizationRoot _authorizationRoot;
+
+        public AuthorizationController(AuthorizationRoot authorizationRoot)
+        {
+            _authorizationRoot = authorizationRoot;
+        }
+
+        public IEnumerable<LoginInfoModel> Index()
+        {
+            var model = _authorizationRoot.Clients.Select(client => new LoginInfoModel
+            {
+                ProviderName = client.Name
+            });
+            return model;
+        }
+
+        public ActionResult GetAuthUrl(string providerName)
+        {
+            return Json( new { Url = GetClient(providerName).GetLoginLinkUri()}, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Auth(string providerName)
+        {
+            return View(GetClient(providerName).GetUserInfo(Request.QueryString));
+        }
+
+        private IClient GetClient(string providerName)
+        {
+            return _authorizationRoot.Clients.First(c => c.Name == providerName);
+        }
     }
 }
