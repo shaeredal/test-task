47ee3eaccaf8d0bfa7f4a2b50f3cf0b86057df99
shaeredal <shaeredal@gmail.com>
2016-06-20 17:31:29 +0300

    Filter add.

--- OnlinerNotifier/OnlinerNotifier.Web/Controllers/ProductController.cs
+++ OnlinerNotifier/OnlinerNotifier.Web/Controllers/ProductController.cs
@@ -1,11 +1,11 @@
-﻿using System.Linq;
-using System.Net.Http;
-using System.Web.Http;
+﻿using System.Web.Http;
 using OnlinerNotifier.BLL.Models;
 using OnlinerNotifier.BLL.Services;
+using OnlinerNotifier.Filters;
 
 namespace OnlinerNotifier.Controllers
 {
+    [Authentication]
     public class ProductController : ApiController
     {
         private IProductService productService;
@@ -17,15 +17,8 @@ namespace OnlinerNotifier.Controllers
 
         public void Post(ProductViewModel product)
         {
-            var userIdCookie = Request.Headers.GetCookies("User").FirstOrDefault();
-            if (userIdCookie != null)
-            {
-                int userId;
-                if (int.TryParse(userIdCookie["User"].Value, out userId))
-                {
-                    productService.Add(product, userId);
-                }
-            }
+            var userId = ((Principal) RequestContext.Principal).Id;
+            productService.Add(product, userId);
         }
     }
 }
