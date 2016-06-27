dd003f87a3e977aa5fc6caa5fec7c48fe4c99c2f
shaeredal <shaeredal@gmail.com>
2016-06-27 18:31:22 +0300

    Optimize.

--- OnlinerNotifier/OnlinerNotifier.BLL Tests/Validators/EmailValidatorTest.cs
+++ OnlinerNotifier/OnlinerNotifier.BLL Tests/Validators/EmailValidatorTest.cs
@@ -6,26 +6,15 @@ namespace OnlinerNotifier.BLL_Tests.Validators
     [TestFixture]
     public class EmailValidatorTest
     {
-        [Test]
-        public void IsValid_Email_True()
+        [TestCase("OnlinerNotifier@gmail.com", ExpectedResult = true)]
+        [TestCase("OnlinerNotifier@gmailcom", ExpectedResult = false)]
+        public bool IsValid_Email_Validity(string email)
         {
             var emailValidator = new EmailValidator();
-            var email = "OnlinerNotifier@gmail.com";
 
             var result = emailValidator.IsValid(email);
 
-            Assert.IsTrue(result);
-        }
-
-        [Test]
-        public void IsValid_Email_False()
-        {
-            var emailValidator = new EmailValidator();
-            var email = "OnlinerNotifier@gmailcom";
-
-            var result = emailValidator.IsValid(email);
-
-            Assert.IsFalse(result);
+            return result;
         }
     }
 }
