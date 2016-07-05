1d19b503e9563c0ffdea41475dc79bd3b8e536c2
shaeredal <shaeredal@gmail.com>
2016-07-05 17:51:23 +0300

    Install Redis.

---
+++ OnlinerNotifier/OnlinerNotifier.BLL/Redis/RedisConnectorHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace OnlinerNotifier.BLL.Redis
+{
+    public class RedisConnectorHelper
+    {
+        private static Lazy<ConnectionMultiplexer> lazyConnection;
+
+        public static ConnectionMultiplexer Connection => lazyConnection.Value;
+
+        static RedisConnectorHelper()
+        {
+            lazyConnection = new Lazy<ConnectionMultiplexer>(() => ConnectionMultiplexer.Connect("localhost"));
+        }
+    }
+}
