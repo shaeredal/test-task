9900c89d46d3f98b0787d0217596568e7e339d35
shaeredal <shaeredal@gmail.com>
2016-07-07 13:05:59 +0300

    Add small fixes and refactor.

--- OnlinerNotifier/OnlinerNotifier.Web/app/services/currencyService.js
+++ OnlinerNotifier/OnlinerNotifier.Web/app/services/currencyService.js
@@ -1,6 +1,6 @@
 ﻿app.factory('currencies', function($http, $filter) {
-    var currencies = [{ name: "BYB", rate: 1 }];
-    currencies.push({ name: "BYN", rate: 10000 });
+    var currencies = [{ name: "BYN", rate: 10000 }];
+    currencies.push({ name: "BYB", rate: 1 });
     $http.get('https://www.nbrb.by/API/ExRates/Rates/145?onDate=' +
             $filter('date')(Date.now(), 'EEE%2C+dd+MMM+yyyy+21%3A00%3A00') +
             '&Periodicity=0&Cur_ID=145')
